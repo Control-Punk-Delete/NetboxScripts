3c777ef008bb53eae75e89c95115ef0a1409cf0f
Andrii K <xxix0@icloud.com>
2026-01-02 21:49:38 +0200

    Import change

--- dns_resolver.py
+++ dns_resolver.py
@@ -1,11 +1,15 @@
 
-from dcim.models import IPAddress
-from extras.scripts import Script, StringVar
+from extras.scripts import *
+from utilities.exceptions import AbortScript
+
 
 class DnsResolve(Script):
      
-    dns_record = StringVar("DNS Record", description="DNS record to resolve for IP addresses")
+    dns_record = StringVar(description="DNS record to resolve for IP addresses")
 
     def run(self):
+
+        if not self.dns_record:
+            raise AbortScript("Empty DNS record provided")
         self.log_success("Hello World", self.dns_record)
 
