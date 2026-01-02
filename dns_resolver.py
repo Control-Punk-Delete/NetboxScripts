3ae2484a722dfa052e75858d1146d4a17761ffce
Andrii K <xxix0@icloud.com>
2026-01-02 22:30:08 +0200

    Hot fix

--- dns_resolver.py
+++ dns_resolver.py
@@ -8,7 +8,4 @@ class DnsResolve(Script):
     dns_record = StringVar(description="DNS record to resolve for IP addresses")
 
     def run(self, data, commit):
-
-        if data['dns_record'] == "test":
-            raise AbortScript("Empty DNS record provided")
-        self.log_success(f"Hello World: {data['dns_record']}")
+        self.log_success(f"Hello World: {data}")
