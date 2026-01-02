f847d2adef97f190fcbe4009573f50f7b60cc0da
Andrii K <xxix0@icloud.com>
2026-01-02 23:00:46 +0200

    Hot fix

--- dns_resolver.py
+++ dns_resolver.py
@@ -8,4 +8,4 @@ class DnsResolve(Script):
     dns_record = StringVar(description="DNS record to resolve for IP addresses")
 
     def run(self, data, commit):
-        self.log_success(f"Hello World: {data['dns_record']}, {data}")
+        self.log_success(f"Hello World: {data}")
