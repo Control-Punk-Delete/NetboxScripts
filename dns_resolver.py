37dea1a18cf3578932707b910b244a296c921767
Andrii K <xxix0@icloud.com>
2026-01-03 01:43:19 +0200

    Hot Fix

--- dns_resolver.py
+++ dns_resolver.py
@@ -20,7 +20,7 @@ class DnsResolve(Script):
     def ip_exists(self, ip_address):  
         return IPAddress.objects.filter(address=str(ip_address)).exists() 
     
-    def create_ip_address(self, ip_address, **kwargs):  
+    def create_ip_address(self, ip_address, commit, **kwargs):  
         """Create new IP address with optional parameters"""  
         ip = IPAddress(  
             address=str(ip_address),  
@@ -44,6 +44,7 @@ class DnsResolve(Script):
 
         ip_address_ids = []
 
+
         for ip_to_check in resolved_ips:
             if self.ip_exists(ip_to_check):  
                 self.log_success(f"IP {ip_to_check} already exists in database")  
@@ -52,7 +53,7 @@ class DnsResolve(Script):
 
             else:  
                 self.log_info(f"Creating IP {ip_to_check}")  
-                new_ip = self.create_ip_address(ip_to_check, commit=commit)  
+                new_ip = self.create_ip_address(ip_to_check, commit=commit, tenant=data['tenant']['id'])  
                 if commit:  
                     self.log_success(f"Created IP {new_ip.address} (ID: {new_ip.id})")
                     ip_address_ids.append(new_ip.id)
