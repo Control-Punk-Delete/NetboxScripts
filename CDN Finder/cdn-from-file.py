e6b8cbf2282694b6b5e2b14b9e55b823e379d361
Control-Punk-Delete <cyber.resilience.ua@gmail.com>
2026-01-05 11:58:24 +0000

    TEst

---
+++ CDN Finder/cdn-from-file.py
@@ -0,0 +1,52 @@
+import ipaddress
+from extras.scripts import *
+from glob import glob
+from itertools import chain
+
+
+class CDNCreation(Script):
+    CDN_IPSv4_LIST = []
+    CDN_IPSv6_LIST = []
+
+
+    def cdn_list_creator(self):
+        self.log_debug("Start cdn_list_creator function")
+        for line in (
+            line.strip()
+            for line in chain.from_iterable(open(f) for f in glob("cdn-lists/ipv4networks-*"))
+        ):
+            if not line or line.startswith("#"):
+                self.log_debug("Not a line")
+                continue
+            try:
+                self.log_debug(f"Populare a {line}")
+                net = ipaddress.ip_network(line, strict=False)
+                if isinstance(net, ipaddress.IPv4Network):
+                  self.CDN_IPSv4_LIST.append(net)
+            except ValueError:
+                pass
+   
+        for line in (
+            line.strip()
+            for line in chain.from_iterable(open(f) for f in glob("cdn-lists/ipv6networks-*"))
+        ):
+            if not line or line.startswith("#"):
+                continue
+            try:
+                net = ipaddress.ip_network(line, strict=False)
+                if isinstance(net, ipaddress.IPv6Network):
+                    self.CDN_IPSv6_LIST.append(net)
+            except ValueError:
+                pass
+        self.log_debug("END cdn_list_creator function")
+        
+
+    def run(self, data, commit):
+        
+        self.log_info(f"Script is starting")
+        self.log_info(f"Start running cdn list creation")
+
+        self.cdn_list_creator()
+
+        self.log_info(f"CDN List v4: {self.CDN_IPSv4_LIST}")
+        self.log_info(f"CDN List v6: {self.CDN_IPSv6_LIST}")
