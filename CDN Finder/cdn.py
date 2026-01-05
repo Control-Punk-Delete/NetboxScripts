0ff3b7b7a95779ffdd21a6b96acfd7a20b524629
Control-Punk-Delete <cyber.resilience.ua@gmail.com>
2026-01-05 10:37:06 +0000

    Test run

--- CDN Finder/cdn.py
+++ CDN Finder/cdn.py
@@ -6,38 +6,38 @@ class IPValidator(Scripts):
     CDN_IPSv4_LIST = []
     CDN_IPSv6_LIST = []
 
-    def cdn_list_vreator(self):
-        for line in (
-            line.strip()
-            for line in chain.from_iterable(open(f) for f in glob("cdn-lists/ipv4networks-*"))
-        ):
-            if not line or line.startswith("#"):
-                continue
-            try:
-                net = ipaddress.ip_network(line, strict=False)
-                if isinstance(net, ipaddress.IPv4Network):
-                    self.CDN_IPSv4_LIST.append(net)
-            except ValueError:
-                pass
+    #def cdn_list_vreator(self):
+    #    for line in (
+    #        line.strip()
+    #        for line in chain.from_iterable(open(f) for f in glob("cdn-lists/ipv4networks-*"))
+    #    ):
+    #        if not line or line.startswith("#"):
+    #            continue
+    #        try:
+    #            net = ipaddress.ip_network(line, strict=False)
+    #            if isinstance(net, ipaddress.IPv4Network):
+    #              self.CDN_IPSv4_LIST.append(net)
+    #        except ValueError:
+    #            pass
 
-        for line in (
-            line.strip()
-            for line in chain.from_iterable(open(f) for f in glob("cdn-lists/ipv6networks-*"))
-        ):
-            if not line or line.startswith("#"):
-                continue
-            try:
-                net = ipaddress.ip_network(line, strict=False)
-                if isinstance(net, ipaddress.IPv6Network):
-                    self.CDN_IPSv6_LIST.append(net)
-            except ValueError:
-                pass
+    #    for line in (
+    #        line.strip()
+    #        for line in chain.from_iterable(open(f) for f in glob("cdn-lists/ipv6networks-*"))
+    #    ):
+    #        if not line or line.startswith("#"):
+    #            continue
+    #        try:
+    #            net = ipaddress.ip_network(line, strict=False)
+    #            if isinstance(net, ipaddress.IPv6Network):
+    #                self.CDN_IPSv6_LIST.append(net)
+    #        except ValueError:
+    #            pass
 
     def run(self, data, commit):
         self.log_debug(f"Script is starting")
         self.log_debug(f"Start running cdn list creation")
 
-        self.cdn_list_vreator(self)
+        #self.cdn_list_vreator(self)
 
         self.log_info(f"v4: {self.CDN_IPSv4_LIST}")
         self.log_info(f"v4: {self.CDN_IPSv6_LIST}")
