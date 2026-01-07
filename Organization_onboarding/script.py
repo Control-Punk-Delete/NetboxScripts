c8f0d766ece1f74070a47d70e6bebdb05d59eb06
Control-Punk-Delete <cyber.resilience.ua@gmail.com>
2026-01-07 20:21:17 +0000

    Fix organization onboarding script

--- Organization_onboarding/script.py
+++ Organization_onboarding/script.py
@@ -1,69 +1,78 @@
-from extras.scripts import *
-from tenancy.models import Tenant  
+from extras.scripts import Script, StringVar  
+from tenancy.models import Tenant
+from django.core.exceptions import MultipleObjectsReturned 
 from django.utils.text import slugify  
-from utilities.exceptions import AbortScript  
-  
-class OrganizationOnboarding(Script):  
-    # class Meta(Script.Meta):  
-    #     name = "Organization Information"  
-    #     description = "Collect organization general information and contact details"
-    #     fieldsets = (    
-    #         ('General Information', ('edrpou', 'short_name', 'full_name', 'root_domain')),        
-    #     )
-    class Meta(Script.Meta):
-        name = "Organization Information"
-        description = "Collect organization general information and contact details"
 
-    # General Information  
-    edrpou = StringVar(
-        description="Name of the new site", required=True
-    ) 
-    short_name = StringVar( description="Short name", required=True )  
-    # full_name = StringVar(  
-    #     description="Full name",  
-    #     required=True  
-    # )
+from utilities.exceptions import AbortScript
+
     
-    # root_domain = StringVar(  
-    #     description="Root domain",  
-    #     required=False  
-    # )  
+class OrganizationOnboarding(Script):  
+    class Meta(Script.Meta):  
+        name = "Organization Information"  
+        description = "Collect organization general information and contact details"  
+        fieldsets = (  
+            ('General Information', ('edrpou', 'short_name', 'full_name', 'root_domain')),  
+            ('Contact Information', ('contact_name', 'email', 'phone')),  
+        )  
       
-    # # Contact Information  
-    # contact_name = StringVar(  
-    #     description="Contact name",  
-    #     required=False  
-    # )  
-    # email = StringVar(  
-    #     description="Email address",  
-    #     required=False  
-    # )  
-    # phone = StringVar(  
-    #     description="Phone number",  
-    #     required=False  
-    # )  
+    # General Information  
+    edrpou = StringVar(  
+        description="EDRPOU",  
+        required=True
+    )  
+    short_name = StringVar(  
+        description="Short name",  
+        required=True  
+    )  
+    full_name = StringVar(  
+        description="Full name",  
+        required=True  
+    )  
+    root_domain = StringVar(  
+        description="Root domain",  
+        required=True  
+    )  
+      
+    # Contact Information  
+    contact_name = StringVar(  
+        description="Contact name",  
+        required=True  
+    )  
+    email = StringVar(  
+        description="Email address",  
+        required=False  
+    )  
+    phone = StringVar(  
+        description="Phone number",  
+        required=False  
+    )
   
-    def run(self, data, commit):  
-        edrpou = data['edrpou']  
-        #short_name = data['short_name']  
-        #full_name = data['full_name']
+  
 
-        self.log_debug(f"Edrpou {edrpou}")
-          
-        # if Tenant.objects.filter(custom_field_data__edrpou=edrpou).exists():  
-        #     raise AbortScript(f"Tenant with edrpou {edrpou} already exist.")  
-          
-        # tenant = Tenant.objects.create(  
-        #     name=short_name,  
-        #     slug=slugify(short_name),  
-        #     custom_field_data={  
-        #         'edrpou': edrpou,  
-        #         'full_name': full_name  
-        #     }  
-        # )  
-          
-        # if commit:  
-        #     tenant.save()  
-        #     self.log_success(f"Tenant created: {tenant}")  
-          
-        # return tenant
+    def run(self, data, commit):  
+        # Access the form data  
+        edrpou = data['edrpou']  
+        short_name = data['short_name']  
+        full_name = data['full_name']  
+        root_domain = data['root_domain']  
+        contact_name = data['contact_name']  
+        #email = data.get('email', '')  
+        #phone = data.get('phone', '')
+
+        
+        if Tenant.objects.filter(custom_field_data__edrpou=edrpou).exists():
+            raise AbortScript(f"Tenant with edrpou { edrpou } alredy exist.")
+
+            
+        self.log_debug("Create a Tenant object")
+
+        # Full tenant creation with all fields  
+        tenant = Tenant.objects.create(  
+        name=short_name,  
+        slug=slugify(short_name),   
+        custom_field_data={  
+                'edrpou': edrpou,  
+                'full_name': full_name  
+                }  
+            )
+        self.log_debug(f"Createed Tenant {tenant}")
