2d9db11774a96ba22677073d74d7936cfe2b693d
Control-Punk-Delete <cyber.resilience.ua@gmail.com>
2026-01-08 12:11:57 +0000

    Rebase zone creating

--- Organization_onboarding/script.py
+++ Organization_onboarding/script.py
@@ -100,7 +100,7 @@ class OrganizationOnboarding(Script):
             zone = Zone.objects.create(name=domain_zone,  
                                     status=ZoneStatusChoices.STATUS_ACTIVE,  
                                     tenant=tenant,  
-                                    soa_mname=[ns],
+                                    soa_mname=ns,
                                     soa_rname=domain_zone,
                                     **Zone.get_defaults()
                                     )
