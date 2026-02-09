683ccac8043cd86b79148e58e2904e8df9dabd85
Control-Punk-Delete <cyber.resilience.ua@gmail.com>
2026-02-09 09:10:40 +0000

    Hot fix

--- DNS_Resolver/dns_resolver.py
+++ DNS_Resolver/dns_resolver.py
@@ -461,7 +461,7 @@ class DnsResolve(Script):
             if ip.is_link_local:
                 TAGS.append("link-local")
 
-            if is_cdn(ip):
+            if self.is_cdn(self, ip=ip):
                 TAGS.append("cdn")
 
             return TAGS
