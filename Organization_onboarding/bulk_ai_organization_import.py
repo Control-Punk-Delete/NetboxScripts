6067d76054370261c08cae1dcce19033949ee9db
Control-Punk-Delete <cyber.resilience.ua@gmail.com>
2026-02-23 14:56:51 +0000

    Hot Fix

--- Organization_onboarding/bulk_ai_organization_import.py
+++ Organization_onboarding/bulk_ai_organization_import.py
@@ -40,7 +40,7 @@ class BulkOrganizationOnboarding(Script):
         self.log_info(f"Processed {created_tenants} tenants.")  
   
     def _process_row(self, row, commit):
-        self.log_debug(f"row")
+        self.log_debug(f"{row}")
         edrpou = row['edrpou'].strip()  
         short_name = row['short_name'].strip()  
         full_name = row['full_name'].strip()  
