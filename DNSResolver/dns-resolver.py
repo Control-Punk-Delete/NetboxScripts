5b59df150f40866d25c2d1abcc91532d4c868519
Control-Punk-Delete <cyber.resilience.ua@gmail.com>
2026-05-04 12:50:56 +0000

    version 1

--- DNSResolver/dns-resolver.py
+++ DNSResolver/dns-resolver.py
@@ -26,6 +26,49 @@ class DnsResolve(Script):
     def run(self, data, commit):
 
         self.log_info(data)
+        
+        # Отримуємо обʼєкт ДНС запису для подальшого його зміни
+        dns_record_object = Record.objects.get(pk=data.get('id', None))
 
+        # Отримуємо рядок ДНС для виконання резолву
+        dns_record_str = data.get('fqdn')[:-1]
         
+        # Отримуємо Ідентифікатор Тенанта, щоб використатит його при створені ІР адрес які не існують
+        if data.get('tenant'):
+            tenant_id = data.get('tenant').get('id')
+
+        # Виконання резолву домена в ІР
+        resolved_ips = self.resolve_dns_record(dns_record_str)
+
+        # Ініціалізація переліку ІД ІР обєктів, які необхідно привʼязати до ДНС запису 
+        resolved_ips_id = []
+
+        self.log_debug(f"Find {len(resolved_ips)} ip addresses")
+
+        # Якщо резолв не вийшов - змінюємо статус домена на - inactive.
+        if not resolved_ips:
+            dns_record_object.status = "inactive"
+            dns_record_object.save()
+            self.log_success(f"DNS Record {dns_record_str} has no resolved IP Address")
         
+        else:
+            # Перевірка чи створені обʼєкти ІР, якщо ні - створюємо 
+            for ip in resolved_ips:
+                ipaddr, created = IPAddress.objects.get_or_create( address= ip ,  
+                                                                defaults={ 'status': 'active',
+                                                                            'tenant':  Tenant.objects.get(pk=tenant_id)} )
+                
+                # Для кожного ІР привʼязуємо домен який виконав резолв
+                ipaddr.custom_field_data['domains'].append(dns_record_object.id)
+                if created:
+                    self.log_debug(f"IP Address {ip} - created")
+
+                ipaddr.save()
+
+                # Зберігаємо ІД ІР обʼєкту
+                resolved_ips_id.append(ipaddr.id)
+            
+            
+            dns_record_object.custom_field_data['ip_address'] = list(set(resolved_ips_id + [ for ip in data.get('custom_fields', {'ip_address': []} ).get('ip_address') ]))
+            dns_record_object.save()        
+
